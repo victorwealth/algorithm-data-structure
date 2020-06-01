11ce3c67bc465301aec0f1787a85c1ed9e1654a3
Victor Akpan <v.akpan@live.com>
2020-06-01 11:59:38 +0400

    added test cases

--- algorithm/palindrome.js
+++ algorithm/palindrome.js
@@ -11,6 +11,18 @@ We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and 
 
 
 TEST CASES 
+1. palindrome("eye") should return true.
+2. palindrome("_eye") should return true.
+3. palindrome("race car") should return true.
+4. palindrome("not a palindrome") should return false.
+5. palindrome("A man, a plan, a canal. Panama") should return true.
+6. palindrome("never odd or even") should return true.
+7. palindrome("nope") should return false.
+8. palindrome("almostomla") should return false.
+9. palindrome("My age is 0, 0 si ega ym.") should return true.
+10. palindrome("1 eye for of 1 eye.") should return false.
+11. palindrome("0_0 (: /-\ :) 0-0") should return true.
+12. palindrome("five|\_/|four") should return false.
 
 */
 
